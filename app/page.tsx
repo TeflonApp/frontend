f14c3fdc37e09a8c320aa11acb2042ea0acdf623
Thorben Bender <bender.thorben0@gmail.com>
2023-07-13 18:28:37 +0200

    remove sql

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import { sql } from "@vercel/postgres";
 
 export default async function Home() {
-  const { rows } = await sql`SELECT * from CARTS`;
   return (
     <main className="flex min-h-screen flex-col items-center flex-start p-24">
       <section w-full>
